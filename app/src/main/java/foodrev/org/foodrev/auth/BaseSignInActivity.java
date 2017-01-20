09adb7bc09e95a0277b14de4a5a995004f1a3318
George Jone <george@Georges-MacBook-Pro.local>
2017-01-20 14:13:11 -0800

    added authentication module and ui resources

---
+++ app/src/main/java/foodrev/org/foodrev/auth/BaseSignInActivity.java
@@ -0,0 +1,36 @@
+package foodrev.org.foodrev.auth;
+
+import android.app.ProgressDialog;
+import android.support.annotation.VisibleForTesting;
+import android.support.v7.app.AppCompatActivity;
+
+import foodrev.org.foodrev.R;
+
+public class BaseSignInActivity extends AppCompatActivity {
+
+    @VisibleForTesting
+    public ProgressDialog mProgressDialog;
+
+    public void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage(getString(R.string.loading));
+            mProgressDialog.setIndeterminate(true);
+        }
+
+        mProgressDialog.show();
+    }
+
+    public void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        hideProgressDialog();
+    }
+
+}
