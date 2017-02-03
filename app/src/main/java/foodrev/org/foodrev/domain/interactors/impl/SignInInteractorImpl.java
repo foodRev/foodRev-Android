7ec2970cce07b3d3a9d7aaff21f659f7736bbaa3
darver <darinvergara@gmail.com>
2017-02-03 13:14:18 -0800

    cleaned up SignInInteractorImpl

--- app/src/main/java/foodrev/org/foodrev/domain/interactors/impl/SignInInteractorImpl.java
+++ app/src/main/java/foodrev/org/foodrev/domain/interactors/impl/SignInInteractorImpl.java
@@ -1,12 +1,9 @@
 package foodrev.org.foodrev.domain.interactors.impl;
 
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.firebase.auth.FirebaseAuth;
 import foodrev.org.foodrev.domain.executor.Executor;
 import foodrev.org.foodrev.domain.executor.MainThread;
 import foodrev.org.foodrev.domain.interactors.SignInInteractor;
 import foodrev.org.foodrev.domain.interactors.base.AbstractInteractor;
-
 
 /**
  * Created by darver on 1/26/17.
@@ -16,16 +13,12 @@ public class SignInInteractorImpl extends AbstractInteractor implements SignInIn
 
     private static final String TAG = "SignInInteractorImpl";
     private SignInInteractor.Callback mCallback;
-    private FirebaseAuth mAuth;
-    private FirebaseAuth.AuthStateListener mAuthListener;
-    private GoogleApiClient mGoogleApiClient;
+
 
     public SignInInteractorImpl(Executor executor, MainThread mainThread, Callback callback) {
         super(executor, mainThread);
         mCallback = callback;
     }
-
-
 
     public void run() {
 
