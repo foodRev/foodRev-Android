899990b9fb11e109e9a10dfc0f9a78ca8a8af6ca
darver <darinvergara@gmail.com>
2017-02-07 12:14:35 -0800

    renamed GoogleSignInActivity

--- app/src/main/java/foodrev/org/foodrev/presentation/ui/activities/SignInActivity.java
+++ app/src/main/java/foodrev/org/foodrev/presentation/ui/activities/SignInActivity.java
@@ -19,35 +19,19 @@ package foodrev.org.foodrev.presentation.ui.activities;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.ActionMode;
 import android.view.View;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
-import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Status;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
 
 import foodrev.org.foodrev.R;
 import foodrev.org.foodrev.Threading.MainThreadImpl;
-import foodrev.org.foodrev.domain.executor.Executor;
 import foodrev.org.foodrev.domain.executor.impl.ThreadExecutor;
 import foodrev.org.foodrev.domain.wrappers.GoogleAuthProviderWrapper;
 import foodrev.org.foodrev.presentation.presenters.SignInPresenter;
@@ -55,10 +39,10 @@ import foodrev.org.foodrev.presentation.presenters.impl.SignInPresenterImpl;
 import foodrev.org.foodrev.presentation.ui.activities.rapidprototype.MainActivity;
 
 
-public class GoogleSignInActivity extends AppCompatActivity implements SignInPresenter.View, View.OnClickListener {
+public class SignInActivity extends AppCompatActivity implements SignInPresenter.View, View.OnClickListener {
 
 
-    private static final String TAG = "GoogleSignInActivity";
+    private static final String TAG = "SignInActivity";
     private static final int RC_SIGN_IN = 9001;
     private SignInPresenter mPresenter;
     private String mDefaultWebClientId;
@@ -105,7 +89,7 @@ public class GoogleSignInActivity extends AppCompatActivity implements SignInPre
     }
 
     private GoogleApiClient setupGoogleSignIn(Context context) {
-        String defaultWebClientId = ((GoogleSignInActivity)context).getDefaultWebClientId();
+        String defaultWebClientId = ((SignInActivity)context).getDefaultWebClientId();
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(defaultWebClientId)
                 .requestEmail()
