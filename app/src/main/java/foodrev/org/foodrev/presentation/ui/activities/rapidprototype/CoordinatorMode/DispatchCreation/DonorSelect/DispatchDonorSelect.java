0889299ee7b9e4d48642a742f999894302155a14
Gregory Kielian <greg@foodrev.org>
2017-04-08 12:55:40 -0700

    add highlight for pre-selected donors

--- app/src/main/java/foodrev/org/foodrev/presentation/ui/activities/rapidprototype/CoordinatorMode/DispatchCreation/DonorSelect/DispatchDonorSelect.java
+++ app/src/main/java/foodrev/org/foodrev/presentation/ui/activities/rapidprototype/CoordinatorMode/DispatchCreation/DonorSelect/DispatchDonorSelect.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -16,6 +17,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -27,6 +29,12 @@ import static foodrev.org.foodrev.domain.models.dispatchModels.Dispatch.Dispatch
 import static java.security.AccessController.getContext;
 
 public class DispatchDonorSelect extends AppCompatActivity {
+
+    // for pre-selection highlight
+    private ArrayList<String> chosenItems;
+    private ValueEventListener communityListener;
+
+
 
     // prior Intent
     Intent dispatchCreateIntent;
@@ -72,6 +80,8 @@ public class DispatchDonorSelect extends AppCompatActivity {
         //setup Firebase must come after adapter is set up
         // and initialize donor array
         setupFirebase();
+        getExistingList();
+        populateList();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -108,37 +118,72 @@ public class DispatchDonorSelect extends AppCompatActivity {
         // donor Root
         donorRoot = firebaseDatabase.getReference("/DONORS");
 
-        // note: this will also do the initial population of the list as well
-        donorRoot.addChildEventListener(new ChildEventListener() {
-            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+        // note: this will also do the initial population of the list as well donorRoot.addChildEventListener(new ChildEventListener() {
+    }
 
-                // update the client-side model
-                dispatchDonors.add( 0, new DispatchDonor(
-                        dataSnapshot.getKey().toString(),
-                        dataSnapshot.child("donorName").getValue().toString(),
-                        Integer.parseInt(dataSnapshot.child("carsOfFood").getValue().toString()),
-                        false));
-
-                // update the UI
-                donorSelectAdapter.notifyItemInserted(0);
-
+    private void getExistingList() {
+        // init or re-init the chosenItems array
+        chosenItems = new ArrayList<>();
+        dispatchRoot.child(dispatchKey).child("DONORS").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                //get existing list into the chosen items array
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                   chosenItems.add(snapshot.getKey().toString());
+                }
             }
 
-            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-            }
-
-            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-                Toast.makeText(DispatchDonorSelect.this, "child changed", Toast.LENGTH_SHORT).show();
-            }
-
-            public void onChildRemoved(DataSnapshot dataSnapshot) {
-                Toast.makeText(DispatchDonorSelect.this, "child removed", Toast.LENGTH_SHORT).show();
-            }
-
-            public void onCancelled(DatabaseError e) {
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.e("DispatchDonorSelect", "onCancelled: " + databaseError.getMessage() );
             }
         });
     }
+
+        private void populateList() {
+            // note: this will also do the initial population of the list as well
+            // update the client-side model
+// update the UI
+            donorRoot.addListenerForSingleValueEvent(new ValueEventListener() {
+
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    for (DataSnapshot snapshot: dataSnapshot.getChildren()){
+                        String itemKey = snapshot.getKey().toString();
+                        Boolean isAlreadySelected = false;
+
+                        for (String chosenItem : chosenItems) {
+                            if(itemKey.equals(chosenItem)) {
+                                isAlreadySelected = true;
+                                break;
+                            }
+                        }
+
+                        // update the client-side model
+
+                        dispatchDonors.add( 0, new DispatchDonor(
+                                snapshot.getKey().toString(),
+                                snapshot.child("donorName").getValue().toString(),
+                                Integer.parseInt(snapshot.child("carsOfFood").getValue().toString()),
+                                isAlreadySelected));
+
+                        // update the UI
+                        donorSelectAdapter.notifyItemInserted(0);
+
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    // TODO create tags for logging errors instead of hardcoding
+                    Log.e("DispatchDonorSelect", "onCancelled: " + databaseError.getMessage());
+
+                }
+
+
+            });
+    }
+
 
 
 }
