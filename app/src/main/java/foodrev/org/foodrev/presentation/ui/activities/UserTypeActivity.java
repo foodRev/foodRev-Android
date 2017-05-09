5ecf29af183c795b81504967078191466b68a5fe
darver <darinvergara@gmail.com>
2017-05-09 16:13:57 -0700

    added UserTypeActivity

---
+++ app/src/main/java/foodrev/org/foodrev/presentation/ui/activities/UserTypeActivity.java
@@ -0,0 +1,15 @@
+package foodrev.org.foodrev.presentation.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import foodrev.org.foodrev.R;
+
+public class UserTypeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_type);
+    }
+}
