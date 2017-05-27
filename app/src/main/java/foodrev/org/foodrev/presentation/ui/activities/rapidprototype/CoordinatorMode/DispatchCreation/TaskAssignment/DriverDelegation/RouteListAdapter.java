12fdde4944e381950d55fdec36e6d2677ec0c755
Gregory Kielian <greg@foodrev.org>
2017-05-27 09:29:34 -0700

    created route list adapter for route recycler view

--- app/src/main/java/foodrev/org/foodrev/presentation/ui/activities/rapidprototype/CoordinatorMode/DispatchCreation/TaskAssignment/DriverDelegation/RouteListAdapter.java
+++ app/src/main/java/foodrev/org/foodrev/presentation/ui/activities/rapidprototype/CoordinatorMode/DispatchCreation/TaskAssignment/DriverDelegation/RouteListAdapter.java
@@ -1,9 +1,14 @@
 package foodrev.org.foodrev.presentation.ui.activities.rapidprototype.CoordinatorMode.DispatchCreation.TaskAssignment.DriverDelegation;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,6 +17,7 @@ import java.util.ArrayList;
 import foodrev.org.foodrev.R;
 import foodrev.org.foodrev.domain.models.dispatchModels.DispatchDonor;
 import foodrev.org.foodrev.domain.models.dispatchModels.DonorToCommunityPair;
+import foodrev.org.foodrev.presentation.ui.activities.rapidprototype.CoordinatorMode.DispatchCreation.TaskAssignment.DonorCommunityPairCreation.CommunityAllocation.CommunityAllocationList;
 
 /**
  * Created by dastechlabs on 5/24/17.
@@ -19,105 +25,89 @@ import foodrev.org.foodrev.domain.models.dispatchModels.DonorToCommunityPair;
 
 public class RouteListAdapter extends RecyclerView.Adapter<RouteListAdapter.ViewHolder> {
 
-        private ArrayList<DonorToCommunityPair> dispatchDCPair;
+        private ArrayList<DonorToCommunityPair> dispatchRoutes;
         private Context context;
 
         // define viewholder
         public static class ViewHolder extends RecyclerView.ViewHolder {
-            public ImageView donorCommunityIcon;
-            public TextView donorCommunityTextView;
-            public TextView donorCommunityTotalCarsOfFood;
-            public TextView donorCommunityAllocatedCarsOfFood;
-            public CardView donorCommunityCardView;
+            public ImageView routeIcon;
+            public TextView routeTextView;
+            public TextView routeTotalCarsOfFood;
+            public TextView routeAllocatedCarsOfFood;
+            public CardView routeCardView;
 
-            public ViewHolder(View donorCommunityItemView) {
-                super(donorCommunityItemView);
+            public ViewHolder(View routeItemView) {
+                super(routeItemView);
 
-                donorCommunityIcon = (ImageView) donorCommunityItemView.findViewById(R.id.route_select_icon);
-                donorCommunityTextView = (TextView) donorCommunityItemView.findViewById(R.id.route_select_name);
-                donorCommunityTotalCarsOfFood = (TextView) donorCommunityItemView.findViewById(R.id.route_total_cars_of_food);
-                donorCommunityAllocatedCarsOfFood = (TextView) donorCommunityItemView.findViewById(R.id.route_allocated_cars_of_food);
-                donorCommunityCardView = (CardView) donorCommunityItemView.findViewById(R.id.dispatch_route_select_card_view);
+                routeIcon = (ImageView) routeItemView.findViewById(R.id.route_select_icon);
+                routeTextView = (TextView) routeItemView.findViewById(R.id.route_select_name);
+                routeTotalCarsOfFood = (TextView) routeItemView.findViewById(R.id.route_select_donation_food_capacity);
+                routeAllocatedCarsOfFood = (TextView) routeItemView.findViewById(R.id.route_select_donation_allocated_total);
+                routeCardView = (CardView) routeItemView.findViewById(R.id.route_card_view);
 
             }
         }
 
         // define how to put these into local array
-        public DonorCommunityPairSelectAdapter(Context context, ArrayList<DispatchDonor> dispatchDonors) {
+        public RouteListAdapter(Context context, ArrayList<DispatchDonor> dispatchDonors) {
             this.context = context;
-            this.dispatchDonors = dispatchDonors;
+            this.dispatchRoutes = dispatchRoutes;
         }
 
 
         // inflates layout from xml and returns the view holder
         @Override
-        public DonorCommunityPairSelectAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        public RouteListAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             Context context = parent.getContext();
             LayoutInflater inflater = LayoutInflater.from(context);
 
-            View dispatchDonorView = inflater.inflate(R.layout.donor_community_pair_select_item, parent, false);
+            View dispatchRouteView = inflater.inflate(R.layout.route_select_item, parent, false);
 
-            ViewHolder viewHolder = new ViewHolder(dispatchDonorView);
+            ViewHolder viewHolder = new ViewHolder(dispatchRouteView);
             return viewHolder;
         }
 
         // populate values into item through holder
         @Override
-        public void onBindViewHolder(final DonorCommunityPairSelectAdapter.ViewHolder viewHolder, int position) {
-            final DispatchDonor dispatchDonor = dispatchDonors.get(position);
+        public void onBindViewHolder(final RouteListAdapter.ViewHolder viewHolder, int position) {
+            final DonorToCommunityPair dispatchRoute = dispatchRoutes.get(position);
 
             // set item views based on xml and model
 
             // set donor name
-            TextView donorNameView = viewHolder.donorNameTextView;
-            donorNameView.setText(dispatchDonor.getDonorName());
+            TextView routeNameView = viewHolder.routeTextView;
+            routeNameView.setText(dispatchRoute.getDispatchDonor().getDonorName()
+                    + " to "
+                    + dispatchRoute.getDispatchCommunity().getCommunityName() );
+            //donorNameView.setText(dispatchRoute.getDispatchCommunity());
 
             // set total amount
-            TextView donorDonationAmountView = viewHolder.donorTotalCarsOfFood;
-            donorDonationAmountView.setText(String.valueOf(dispatchDonor.getCarsOfFood()));
+            TextView routeAmountView = viewHolder.routeTotalCarsOfFood;
+            routeAmountView.setText(String.valueOf(dispatchRoute.getTrunksOfFood()));
 
             // set total amount
-            TextView donorFoodAllocationCountView = viewHolder.donorAllocatedCarsOfFood;
-            donorFoodAllocationCountView.setText(String.valueOf(dispatchDonor.getAllocatedCarsOfFood()));
+            // TODO change to display allocation
+            TextView routeFoodAllocationCountView = viewHolder.routeAllocatedCarsOfFood;
+            routeFoodAllocationCountView.setText(String.valueOf(dispatchRoute.getTrunksOfFood()));
 
             // set icon view
             // TODO create different icons for different donors based on url
-            ImageView donorIcon = viewHolder.donorIcon;
-            donorIcon.setImageResource(R.drawable.ic_donor);
+            ImageView routeIcon = viewHolder.routeIcon;
+            routeIcon.setImageResource(R.drawable.ic_next);
 
             // set card onclicklistener
-            final CardView donorCardView = viewHolder.donorCardView;
+            final CardView routeCardView = viewHolder.routeCardView;
 
-            highlightIfExistsUnallocatedFood(donorCardView, dispatchDonor);
-
-            donorCardView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent intent = new Intent(context, CommunityAllocationList.class);
-                    intent.putExtra("dispatch_key", dispatchDonor.getDispatchKey());
-                    intent.putExtra("donor_key", dispatchDonor.getDonorUid());
-                    intent.putExtra("donor_name", dispatchDonor.getDonorName());
-                    intent.putExtra("donor_total_food", String.valueOf(dispatchDonor.getCarsOfFood()));
-                    context.startActivity(intent);
-                }
-            });
+            // TODO: add onclick listener
 
         }
 
-        private void highlightIfExistsUnallocatedFood(CardView donorCardView, DispatchDonor dispatchDonor) {
-            if (dispatchDonor.getAllocatedCarsOfFood() < dispatchDonor.getCarsOfFood()) {
-                donorCardView.setCardBackgroundColor(Color.CYAN);
-            } else {
-                donorCardView.setCardBackgroundColor(ContextCompat.getColor(context, R.color.cardview_light_background));
-            }
-        }
 
         // necessary, returns total count of items in list
         @Override
         public int getItemCount() {
-            return dispatchDonors.size();
+            return dispatchRoutes.size();
         }
 
 
     }
-}
