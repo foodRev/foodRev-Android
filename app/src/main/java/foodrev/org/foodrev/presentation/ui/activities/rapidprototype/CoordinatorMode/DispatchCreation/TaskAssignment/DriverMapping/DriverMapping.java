4d848177677a7fd846f4195ce78a105480c953f2
Gregory Kielian <greg@foodrev.org>
2017-06-18 23:42:41 -0700

    add polyline generation connecting pick-up/drop-off points for driver
    - also refactored the way we listen to firebase for Driver GPS
      Coordinates: listeners are added and removed per driver on the top
      level "DRIVERS" section of firebase, this ensures that any dispatch
      doesn't get spammed or irrevelant updates from drivers not in the
      dispatch, instead just has the updates from the relevant drivers,
      furthermore only when an individual driver has an update.

--- app/src/main/java/foodrev/org/foodrev/presentation/ui/activities/rapidprototype/CoordinatorMode/DispatchCreation/TaskAssignment/DriverMapping/DriverMapping.java
+++ app/src/main/java/foodrev/org/foodrev/presentation/ui/activities/rapidprototype/CoordinatorMode/DispatchCreation/TaskAssignment/DriverMapping/DriverMapping.java
@@ -4,10 +4,9 @@ import android.arch.lifecycle.LifecycleRegistry;
 import android.arch.lifecycle.LifecycleRegistryOwner;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
-import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -17,8 +16,12 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import foodrev.org.foodrev.R;
@@ -27,10 +30,14 @@ import foodrev.org.foodrev.domain.models.dispatchModels.DispatchDonor;
 import foodrev.org.foodrev.domain.models.dispatchModels.DispatchDriver;
 
 public class DriverMapping extends FragmentActivity
-        implements OnMapReadyCallback, LifecycleRegistryOwner {
+        implements GoogleMap.OnMarkerClickListener, OnMapReadyCallback, LifecycleRegistryOwner {
 
     LifecycleRegistry lifecycleRegistry = new LifecycleRegistry(this);
 
+    //Polyline
+    private Polyline routeLine;
+    private LatLng waypoint;
+    private List<LatLng> waypoints = new ArrayList<>();
 
     private GoogleMap mMap;
     private MapViewModel model;
@@ -45,6 +52,7 @@ public class DriverMapping extends FragmentActivity
 
     HashMap<String,DispatchDriver> driverUidMap = new HashMap<>();
     HashMap<String, Marker> iconDriverUidMap = new HashMap<>();
+    private PolylineOptions polylineOptions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,9 +90,10 @@ public class DriverMapping extends FragmentActivity
         mMap.moveCamera(CameraUpdateFactory.newLatLng(myLatLng));
 
         addIcons();
+        addRouteLine();
     }
 
-    // add donor icons
+    // add map icons
     private void addIcons() {
 
         model = ViewModelProviders.of(this).get(MapViewModel.class);
@@ -99,6 +108,36 @@ public class DriverMapping extends FragmentActivity
 
     }
 
+
+
+    // Route Line Setup
+    private void addRouteLine() {
+        // setup icon click listeners on Marker for route line
+        mMap.setOnMarkerClickListener(this);
+
+        // set polyline -- aka routeLine -- features
+        polylineOptions = new PolylineOptions()
+                .color(Color.BLUE)
+                .geodesic(true);
+
+        // initialize the routeline (won't show until 2 or more points)
+        routeLine = mMap.addPolyline(polylineOptions);
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        // get latlng
+        waypoint = marker.getPosition();
+
+        // add to list
+        waypoints.add(waypoint);
+        routeLine.setPoints(waypoints);
+        return true;
+    }
+
+    // End Route Line setup methods
+
+    // Start Observation methods
     private void observeDrivers() {
         model.getDrivers().observe(this, dispatchDrivers -> {
             Marker mapMarker;
@@ -108,23 +147,21 @@ public class DriverMapping extends FragmentActivity
 
             DispatchDriver dispatchDriver;
 
-
             //remove stale markers
+            for (String iconUid : driverUidMap.keySet()) {
+                if(!dispatchDrivers.containsKey(iconUid)) {
 
-                    for (String iconUid : driverUidMap.keySet()) {
-                        if(!dispatchDrivers.containsKey(iconUid)) {
+                    // remove from driver Uid Map
+                    driverUidMap.remove(iconUid);
 
-                            // remove from driver Uid Map
-                            driverUidMap.remove(iconUid);
+                    // remove icon
+                    mapMarker = iconDriverUidMap.get(iconUid);
+                    mapMarker.remove();
 
-                            // remove icon
-                            mapMarker = iconDriverUidMap.get(iconUid);
-                            mapMarker.remove();
-
-                            // remove from icon hashmap
-                            iconDriverUidMap.remove(iconUid);
-                        }
-                    }
+                    // remove from icon hashmap
+                    iconDriverUidMap.remove(iconUid);
+                }
+            }
                 
             //add new markers, and update existing markers
             for (Map.Entry<String, DispatchDriver> stringDispatchDriverEntry : dispatchDrivers.entrySet()) {
@@ -138,10 +175,12 @@ public class DriverMapping extends FragmentActivity
                 // place marker for first time if not in list
                 if(!driverUidMap.containsKey(driverUid)) {
                     driverUidMap.put(driverUid, dispatchDriver);
-                    iconDriverUidMap.put(driverUid, addMarker(driverLatLng, dispatchDriver));
+                    mapMarker = addMarker(driverLatLng, dispatchDriver);
+                    iconDriverUidMap.put(driverUid, mapMarker);
 
-                    Toast.makeText(this, "placed marker for first time", Toast.LENGTH_SHORT).show();
 
+                    //uncomment for live placement notification
+                    //Toast.makeText(this, "placed marker for first time for " + dispatchDriver.getName(), Toast.LENGTH_SHORT).show();
 
                     // update marker if possible
                 } else {
@@ -152,7 +191,9 @@ public class DriverMapping extends FragmentActivity
                     mapMarker = updateMarker(driverLatLng, mapMarker, dispatchDriver);
 
                     iconDriverUidMap.put(driverUid, mapMarker);
-                    Toast.makeText(this, "moved marker for", Toast.LENGTH_SHORT).show();
+
+                    //uncomment for live update notification
+                    //Toast.makeText(this, "moved marker for " + dispatchDriver.getName(), Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -175,7 +216,7 @@ public class DriverMapping extends FragmentActivity
                     communityUidMap.put(communityUid, dispatchCommunity);
                     iconCommunityUidMap.put(communityUid, addMarker(communityLatLng, dispatchCommunity));
 
-                    Toast.makeText(this, "placed marker for first time", Toast.LENGTH_SHORT).show();
+                    //Toast.makeText(this, "placed marker for first time", Toast.LENGTH_SHORT).show();
                 } else {
                     communityUidMap.put(communityUid, dispatchCommunity);
                     mapMarker = iconCommunityUidMap.get(communityUid);
@@ -183,7 +224,7 @@ public class DriverMapping extends FragmentActivity
                     mapMarker = updateMarker(communityLatLng, mapMarker, dispatchCommunity);
 
                     iconCommunityUidMap.put(communityUid, mapMarker);
-                    Toast.makeText(this, "moved marker for", Toast.LENGTH_SHORT).show();
+                    //Toast.makeText(this, "moved marker for", Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -208,7 +249,7 @@ public class DriverMapping extends FragmentActivity
                     donorUidMap.put(donorUid, dispatchDonor);
                     iconDonorUidMap.put(donorUid, addMarker(donorLatLng, dispatchDonor));
 
-                    Toast.makeText(this, "placed marker for first time", Toast.LENGTH_SHORT).show();
+                    //Toast.makeText(this, "placed marker for first time", Toast.LENGTH_SHORT).show();
                 } else {
                     donorUidMap.put(donorUid,dispatchDonor);
                     mapMarker = iconDonorUidMap.get(donorUid);
@@ -216,12 +257,16 @@ public class DriverMapping extends FragmentActivity
                     mapMarker = updateMarker(donorLatLng, mapMarker, dispatchDonor);
 
                     iconDonorUidMap.put(donorUid, mapMarker);
-                    Toast.makeText(this, "moved marker for", Toast.LENGTH_SHORT).show();
+                    //Toast.makeText(this, "moved marker for", Toast.LENGTH_SHORT).show();
                 }
             }
         });
     }
 
+    // end observer methods
+
+
+    // start marker update methods
     private Marker addMarker(LatLng objectLatLng, MappableObject mappableObject) {
         // set LatLng
         objectLatLng = new LatLng(
@@ -258,6 +303,8 @@ public class DriverMapping extends FragmentActivity
 
         return mapMarker;
     }
+
+    // end marker update methods
 
     @Override
     public LifecycleRegistry getLifecycle() {
